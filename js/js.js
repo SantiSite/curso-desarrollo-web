73362b28dd6a59e5a791c196ff4ccbfe6545e37b
Santiago Restrepo Villada <santiagorv246@gmail.com>
2020-09-24 22:00:15 -0500

    Agregue js para implementar los eventos de click en el burger-menu. Agregue video de youtube

---
+++ js/js.js
@@ -0,0 +1,23 @@
+const menu = document.querySelector('.menu');
+const burgerButton = document.querySelector('#burger-menu');
+const ipad = window.matchMedia('screen and (max-width: 757px)');
+
+ipad.addListener(validation);
+
+function validation(event) {
+    if (event.matches) {
+        burgerButton.addEventListener('click', hideShow);
+    } else {
+        burgerButton.removeEventListener('click', hideShow);
+    }
+    console.log(event.matches);
+}
+
+function hideShow() {
+    if (menu.classList.contains('is-active')) {
+        menu.classList.remove('is-active');
+    } else {
+        menu.classList.add('is-active');
+    }
+}
+
